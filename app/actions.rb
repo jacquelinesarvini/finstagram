6c014d8a337a0b9001cc1bc27436ff12e21474ad
Jacqueline Sarvini <jacquelinesarvini@gmail.com>
2023-10-07 16:03:35 +0000

    Setting up the layout and dynamically adding loops

--- app/actions.rb
+++ app/actions.rb
@@ -7,7 +7,7 @@ def humanized_time_ago(minute_num)
 end
 
 get '/' do
-  finstagram_post_jackie = {
+  @jackie_wants_to_go_to_kz = {
     username: "jackie_wants_to_go_to_kz",
     avatar_url: "https://borgenproject.org/wp-content/uploads/24265027988_9a8af42209_k-1-530x353.jpg",
     photo_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKRtlX7XoEyxmV1EcLFZr5MISFj9g1xQ-JsgmJvXGd0S8I5HyRzqSRDc1iVnV8IOclwQo&usqp=CAU",
@@ -20,10 +20,10 @@ get '/' do
     }]
   }
 
-  finstagram_post_raph = {
+  @raph_wants_to_go_to_jp = {
     username: "raph_wants_to_go_to_jp",
     avatar_url: "https://www.state.gov/wp-content/uploads/2019/04/Japan-2107x1406.jpg",
-    humanized_time_ago: humanized_time_ago(60),
+    humanized_time_ago: humanized_time_ago(65),
     like_count: 0,
     comment_count: 1,
     comments: [{
@@ -32,17 +32,19 @@ get '/' do
     }]
   }
 
-  finstagram_post_jo = {
+  @jo_wants_to_go_to_nz = {
     username: "jo_wants_to_go_to_nz",
     avatar_url: "https://lp-cms-production.imgix.net/2023-02/GettyImages-1172642617.jpg?auto=format&w=1440&h=810&fit=crop&q=75",
-    humanized_time_ago: humanized_time_ago(180),
+    humanized_time_ago: humanized_time_ago(190),
     like_count: 0,
     comment_count: 1,
     comments: [{
       username: "jo_wants_to_go_to_nz",
-      text:"I'm going to New Zealand on Monday -- super excited! Look at these sheep!"
+      text: "I'm going to New Zealand on Monday -- super excited! Look at these sheep!"
     }]
   }
 
-  [finstagram_post_jackie, finstagram_post_raph, finstagram_post_jo].to_s
+  @finstagram_posts = [@jackie_wants_to_go_to_kz, @raph_wants_to_go_to_jp, @jo_wants_to_go_to_nz].to_s
+
+  erb(:index)
 end
